32e429ced883fdb3746a28839b58111c601c87af
svelozaa <65297414+svelozaa@users.noreply.github.com>
2020-05-17 12:54:14 -0500

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,8 +1,7 @@
 numero_files=$(find . -maxdepth 1 -type f | wc -l)
-
+funtion guess {
 echo -n "Number of files"
 read numero_usuario
-
 while [ $numero_usuario -ne $numero_files ];
 do 
   if [[ $numero_usuario -lt $numero_files ]]
@@ -11,10 +10,12 @@ do
   else 
       echo "Your guess is too high"
   fi
-
   echo -n "Number of files"
   read nuevo_ingreso
 let numero_usuario=$nuevo_ingreso
 done
-
-echo "$numero_usuario, is the answer" 
+echo "Congratulations!"
+echo "$numero_usuario, is the answer"
+}
+echo "Welcome, you have to guess de number o files"
+guess
